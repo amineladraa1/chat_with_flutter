47e8371a88a522eb51aba51540102080aecfbbc4
amine <amineladraa67@gmail.com>
2022-11-18 15:52:57 +0100

    before modal

--- lib/screens/rooms.dart
+++ lib/screens/rooms.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
+import 'package:my_chat/screens/users.dart';
 import '../constants/constants.dart';
 import 'package:badges/badges.dart';
 
@@ -25,101 +26,77 @@ class _MyRoomsState extends State<MyRooms> {
         elevation: 0.0,
         title: const Text('Rooms'),
       ),
-      body: Stack(children: [
-        StreamBuilder<List<types.Room>>(
-            initialData: const [],
-            stream: FirebaseChatCore.instance.rooms(),
-            builder: ((context, snapshot) {
-              if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Container(
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.only(
-                    bottom: 200,
-                  ),
-                  child: const Text('No rooms'),
-                );
-              }
-              return Padding(
-                padding: const EdgeInsets.only(top: 10.0),
-                child: ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: ((context, index) {
-                      final room = snapshot.data![index];
-                      return ListTile(
-                        onLongPress: () => setState(() {
-                          selectedIndex.add(index);
-                          isLongedPressed = true;
-                        }),
-                        onTap: isLongedPressed == true
-                            ? () => setState(() {
-                                  if (selectedIndex.contains(index)) {
-                                    selectedIndex.remove(index);
-                                  } else {
-                                    selectedIndex.add(index);
-                                  }
-                                })
-                            : null,
-                        // todo : create a build avatar for the room
-                        selectedTileColor:
-                            const Color.fromARGB(48, 158, 158, 158),
-                        selectedColor: Colors.black54,
-
-                        selected: selectedIndex.contains(index) ? true : false,
-                        leading: Badge(
-                          position:
-                              BadgePosition.bottomEnd(bottom: -4, end: -6),
-                          showBadge:
-                              selectedIndex.contains(index) ? true : false,
-                          elevation: 0,
-                          badgeColor: kBlueGrey,
-                          badgeContent: const Icon(
-                            Icons.check,
-                            size: 12.0,
-                            color: Colors.white,
-                          ),
-                          padding: const EdgeInsets.all(5.0),
-                          borderSide: const BorderSide(
-                            width: 2.0,
-                            color: Color.fromARGB(179, 158, 158, 158),
-                          ),
-                          child: const CircleAvatar(
-                            backgroundColor: kOrange,
-                            radius: 25.0,
-                          ),
-                        ),
-                        title: Text(room.name ?? ''),
-                        subtitle: Text(room.createdAt.toString()),
-                      );
-                    })),
+      body: StreamBuilder<List<types.Room>>(
+          initialData: const [],
+          stream: FirebaseChatCore.instance.rooms(),
+          builder: ((context, snapshot) {
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(
+                  bottom: 200,
+                ),
+                child: const Text('No rooms'),
               );
-            })),
-        DraggableScrollableSheet(
-          initialChildSize: 0.2,
-          minChildSize: 0.15,
-          maxChildSize: 0.8,
-          builder: (BuildContext context, ScrollController scrollController) {
-            return Container(
-              color: Colors.blue[100],
-              child: ListView.builder(
-                controller: scrollController,
-                itemCount: 25,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(title: Text('Item $index'));
-                },
-              ),
-            );
-          },
-        ),
-      ]),
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: ((context, index) {
+                  final room = snapshot.data![index];
+                  return ListTile(
+                    onLongPress: () => setState(() {
+                      selectedIndex.add(index);
+                      isLongedPressed = true;
+                    }),
+                    onTap: isLongedPressed == true
+                        ? () => setState(() {
+                              if (selectedIndex.contains(index)) {
+                                selectedIndex.remove(index);
+                              } else {
+                                selectedIndex.add(index);
+                              }
+                            })
+                        : null,
+                    // todo : create a build avatar for the room
+                    selectedTileColor: const Color.fromARGB(48, 158, 158, 158),
+                    selectedColor: Colors.black54,
 
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {},
-      //   backgroundColor: kBlueGrey,
-      //   child: const Icon(
-      //     Icons.post_add_rounded,
-      //     color: Colors.white,
-      //   ),
-      // ),
+                    selected: selectedIndex.contains(index) ? true : false,
+                    leading: Badge(
+                      position: BadgePosition.bottomEnd(bottom: -4, end: -6),
+                      showBadge: selectedIndex.contains(index) ? true : false,
+                      elevation: 0,
+                      badgeColor: kBlueGrey,
+                      badgeContent: const Icon(
+                        Icons.check,
+                        size: 12.0,
+                        color: Colors.white,
+                      ),
+                      padding: const EdgeInsets.all(5.0),
+                      borderSide: const BorderSide(
+                        width: 2.0,
+                        color: Color.fromARGB(179, 158, 158, 158),
+                      ),
+                      child: const CircleAvatar(
+                        backgroundColor: kOrange,
+                        radius: 25.0,
+                      ),
+                    ),
+                    title: Text(room.name ?? ''),
+                    subtitle: Text(room.createdAt.toString()),
+                  );
+                }));
+          })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, MyUsers.usersId);
+        },
+        backgroundColor: kBlueGrey,
+        child: const Icon(
+          Icons.post_add_rounded,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
--- lib/screens/users.dart
+++ lib/screens/users.dart
@@ -4,25 +4,36 @@ import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 import 'package:my_chat/constants/constants.dart';
 import 'package:my_chat/services/chat_brain.dart';
+import 'package:badges/badges.dart';
 
 import 'package:my_chat/util.dart';
 
-class MyUsers extends StatelessWidget {
+class MyUsers extends StatefulWidget {
   static String usersId = 'MyUsers';
 
-  MyUsers({super.key});
+  const MyUsers({super.key});
+
+  @override
+  State<MyUsers> createState() => _MyUsersState();
+}
+
+class _MyUsersState extends State<MyUsers> {
   ChatBrain chatBrain = ChatBrain();
+  List<int> selectedIndex = [];
+  List<types.User> selectedUsers = [];
+  bool isLongedPressed = false;
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
           systemOverlayStyle: const SystemUiOverlayStyle(),
           backgroundColor: kBlueGrey,
-          title: const Text('All Users'),
+          elevation: 0.0,
+          title: const Text('Select Users'),
         ),
         body: StreamBuilder<List<types.User>>(
           stream: FirebaseChatCore.instance.users(),
           initialData: const [],
-          builder: (context, snapshot) {
+          builder: ((context, snapshot) {
             if (!snapshot.hasData || snapshot.data!.isEmpty) {
               return Container(
                 alignment: Alignment.center,
@@ -38,45 +49,75 @@ class MyUsers extends StatelessWidget {
               itemBuilder: (context, index) {
                 final user = snapshot.data![index];
 
-                return GestureDetector(
-                  onTap: () {
-                    chatBrain.handlePressed(user, context);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
+                return ListTile(
+                  onLongPress: () => setState(() {
+                    selectedIndex.add(index);
+                    isLongedPressed = true;
+                    selectedUsers.add(user);
+                  }),
+                  onTap: isLongedPressed == true
+                      ? () => setState(() {
+                            if (selectedIndex.contains(index)) {
+                              selectedIndex.remove(index);
+                              selectedUsers.remove(user);
+                            } else {
+                              selectedIndex.add(index);
+                              selectedUsers.add(user);
+                            }
+                          })
+                      : null,
+                  selectedTileColor: const Color.fromARGB(48, 158, 158, 158),
+                  selectedColor: Colors.black,
+                  selected: selectedIndex.contains(index) ? true : false,
+                  mouseCursor: MouseCursor.defer,
+                  // leading: chatBrain.buildAvatar(EdgeInsets.zero,
+                  //     const EdgeInsets.only(right: 10), 0.0, user, 25.0),
+                  leading: Badge(
+                    position: BadgePosition.bottomEnd(bottom: -4, end: -6),
+                    showBadge: selectedIndex.contains(index) ? true : false,
+                    elevation: 0,
+                    badgeColor: kBlueGrey,
+                    badgeContent: const Icon(
+                      Icons.check,
+                      size: 12.0,
+                      color: Colors.white,
                     ),
-                    child: ListTile(
-                        mouseCursor: MouseCursor.defer,
-                        leading: chatBrain.buildAvatar(EdgeInsets.zero,
-                            const EdgeInsets.only(right: 10), 0.0, user, 25.0),
-                        title: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(getUserName(user)),
-                            Text(
-                              chatBrain.lastSeenBuilder(
-                                  DateTime.fromMillisecondsSinceEpoch(
-                                          user.lastSeen!)
-                                      .day,
-                                  DateTime.fromMillisecondsSinceEpoch(
-                                              user.lastSeen!)
-                                          .month -
-                                      1),
-                              style: const TextStyle(
-                                fontSize: 12.0,
-                                color: Colors.black45,
-                              ),
-                            )
-                          ],
-                        )),
+                    padding: const EdgeInsets.all(5.0),
+                    borderSide: const BorderSide(
+                      width: 2.0,
+                      color: Color.fromARGB(179, 158, 158, 158),
+                    ),
+                    child: chatBrain.buildAvatar(
+                        EdgeInsets.zero, EdgeInsets.zero, 0.0, user, 25.0),
+                  ),
+                  title: Text(getUserName(user)),
+                  subtitle: Text(
+                    chatBrain.lastSeenBuilder(
+                        DateTime.fromMillisecondsSinceEpoch(user.lastSeen!).day,
+                        DateTime.fromMillisecondsSinceEpoch(user.lastSeen!)
+                                .month -
+                            1),
+                    style: const TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.black45,
+                    ),
                   ),
                 );
               },
             );
-          },
+          }),
         ),
+        floatingActionButton: selectedIndex.isNotEmpty
+            ? FloatingActionButton(
+                onPressed: () {
+                  // FirebaseChatCore.instance.createGroupRoom(name: name, users:selectedUsers);
+                },
+                backgroundColor: kBlueGrey,
+                child: const Icon(
+                  Icons.check,
+                  color: Colors.white,
+                ),
+              )
+            : null,
       );
 }
